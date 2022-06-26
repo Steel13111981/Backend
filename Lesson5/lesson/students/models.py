f3d45ca0d1b35cdc7b6480216948d8c025254235
Steel13111981 <steel13111981@gmail.com>
2022-06-26 11:56:21 +0300

    lesson5-6

--- Lesson5/lesson/students/models.py
+++ Lesson5/lesson/students/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class StudentGroup(models.Model):
     name = models.CharField(verbose_name="Имя", max_length=100)
+    
     students_number = models.DecimalField(max_digits=4, decimal_places=0, 
                                             verbose_name="Кол-во студентов",
                                              blank=True, null=True)
@@ -17,13 +18,13 @@ class StudentGroup(models.Model):
 
 class Student(models.Model):
     name = models.CharField(verbose_name="Имя", max_length=100, 
-                            blank=True, null=True)
+                                blank=True, null=True)
 
     birthday = models.DateField(verbose_name="День рождения", 
-                            null=True, blank=True)
+                                null=True, blank=True)
 
     group = models.ForeignKey(StudentGroup, verbose_name="Группа", on_delete=models.SET_NULL, 
-                            null=True, blank=True)
+                                null=True, blank=True)
 
     def __str__(self):
         return self.name
